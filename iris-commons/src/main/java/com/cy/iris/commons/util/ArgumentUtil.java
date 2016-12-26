7673d36d3d79229a5c2c85bdb5fe4220bbdd69ff
chicc999 <123695646@qq.com>
2016-12-26 16:06:00 +0800

    添加多参数非空校验

--- iris-commons/src/main/java/com/cy/iris/commons/util/ArgumentUtil.java
+++ iris-commons/src/main/java/com/cy/iris/commons/util/ArgumentUtil.java
@@ -12,11 +12,26 @@ public class ArgumentUtil {
 	 * 检测参数是否为null,如果为null则抛出空指针异常
 	 * 否则返回参数
 	 */
-	public static <T> T isNotNull(T arg, String text) {
+	public static <T> T isNotNull(String text,T arg) {
 		if (arg == null) {
 			throw new NullPointerException(text + "is null.");
 		}
 		return arg;
+	}
+
+	/**
+	 * 检测若干参数是否为null,如果为null则抛出空指针异常
+	 * 否则返回参数
+	 */
+	public static <T> void isNotNull(String[] text,T... arg) {
+		if(text.length != arg.length){
+			throw new IllegalArgumentException("text[] 与 args 长度不等");
+		}
+
+		for(int i= 0; i < arg.length ; i++)
+		if (arg == null) {
+			throw new NullPointerException(text[i] + "is null.");
+		}
 	}
 
 	/**
