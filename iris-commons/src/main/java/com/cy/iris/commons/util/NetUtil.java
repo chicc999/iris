8af5a7cffd209a7b8060f0e3a1f08ef88302affb
chicc999 <123695646@qq.com>
2016-12-26 15:38:32 +0800

    网络辅助类,将serverName转成isa

---
+++ iris-commons/src/main/java/com/cy/iris/commons/util/NetUtil.java
@@ -0,0 +1,50 @@
+package com.cy.iris.commons.util;
+
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
+
+/**
+ * 网络辅助类
+ */
+public class NetUtil {
+
+	/**
+	 * 从serverName获取对应的InetSocketAddress.
+	 * serverName格式为xxx_xxx_xxx_xxx,如10_9_1_133
+	 *
+	 * @param address serverName格式的字符串
+	 * @return inetSocketAddress
+	 * @see java.net.InetSocketAddress
+	 *
+	 * @throws UnknownHostException 如果遇到无法转换的serverName
+	 */
+	public static InetSocketAddress serverNameToISA(String address)throws UnknownHostException {
+
+		ArgumentUtil.isNotBlank(address);
+
+		String[] parts = address.split("_");
+		if (parts.length < 5) {
+			throw new RuntimeException();
+		}
+		int port;
+		try {
+			port = Integer.parseInt(parts[parts.length - 1]);
+		} catch (NumberFormatException e) {
+			throw new UnknownHostException();
+		}
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < parts.length - 1; i++) {
+			if (i > 0) {
+				builder.append('.');
+			}
+			builder.append(parts[i]);
+		}
+		String ip = builder.toString();
+
+		InetSocketAddress isa = new InetSocketAddress(InetAddress.getByName(ip), port);
+
+		return isa;
+	}
+
+}
