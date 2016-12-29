3b83cacecfa48824ef2eab51315952f2b94cf5ca
chicc999 <123695646@qq.com>
2016-12-29 14:25:13 +0800

    序列化辅助类

---
+++ iris-commons/src/main/java/com/cy/iris/commons/util/Serializer.java
@@ -0,0 +1,52 @@
+package com.cy.iris.commons.util;
+
+import io.netty.buffer.ByteBuf;
+
+import java.nio.charset.Charset;
+
+/**
+ * 序列化辅助工具
+ */
+public class Serializer {
+	/**
+	 * 写字符串
+	 *
+	 * @param value      字符串
+	 * @param out        输出缓冲区
+	 * @throws java.lang.Exception
+	 */
+	public static void write(final String value, final ByteBuf out)  {
+
+		ArgumentUtil.isNotNull("out",out);
+
+		byte[] bytes = value.getBytes(Charset.forName("UTF-8"));
+
+		if(bytes.length > 1<<31){
+			throw new IllegalArgumentException("too large String");
+		}
+		//value必须小于2G
+		out.writeInt(bytes.length);
+
+		out.writeBytes(bytes);
+
+	}
+
+	/**
+	 * 读字符串
+	 *
+	 * @param in        输入缓冲区
+	 * @throws java.lang.Exception
+	 */
+	public static String read(final ByteBuf in)  {
+
+		ArgumentUtil.isNotNull("in",in);
+
+		int len = in.readInt();
+		byte[] bytes = new byte[len];
+		in.readBytes(bytes);
+		String value= new String(bytes,Charset.forName("UTF-8"));
+
+		return value;
+
+	}
+}
