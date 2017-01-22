7d69432eddee2ce71bf307969f807c49962a57f2
chicc999 <123695646@qq.com>
2017-01-22 16:01:58 +0800

    修改service类

--- iris-commons/src/main/java/com/cy/iris/commons/service/Service.java
+++ iris-commons/src/main/java/com/cy/iris/commons/service/Service.java
@@ -12,69 +12,82 @@ import java.util.concurrent.atomic.AtomicReference;
  * ACTION            sercive:start()                beforeStart()             doStart()            afterStart()
  * STATE    STOPPED -----------------> WILL_START ----------------> STARTING ------------> STARTED
  * <p>
- * ACTION                          sercive:stop()                beforeStop()             doStop()              afterStop()
- * STATE    STARTED/START_FAILED -----------------> WILL_STOP ----------------> STOPPING -------------> STOPPED
+ * ACTION                          sercive:stop()                beforeStop()             doStop()
+ * STATE    STARTED/START_FAILED -----------------> WILL_STOP ----------------> STOPPING -------------> STOPPED/STOP_FAILED
  */
 public abstract class Service implements LifeCycle {
 
 	Logger logger = LoggerFactory.getLogger(Service.class);
 
 	// 是否启动
-	protected final AtomicBoolean started = new AtomicBoolean(false);
+	//protected final AtomicBoolean started = new AtomicBoolean(false);
 
 	// 服务状态
 	protected final AtomicReference<ServiceState> serviceState =
-			new AtomicReference<ServiceState>();
+			new AtomicReference<ServiceState>(ServiceState.STOPPED);
 
 	@Override
 	public final void start() throws Exception {
-		serviceState.set(ServiceState.WILL_START);
-		beforeStart();
-		if (started.compareAndSet(false, true)) {
-			try {
-				serviceState.set(ServiceState.STARTING);
-				doStart();
-				serviceState.set(ServiceState.STARTED);
-				afterStart();
-			} catch (Exception e) {
-				serviceState.set(ServiceState.START_FAILED);
-				logger.error("启动失败", e);
-				stop();
+		if(serviceState.compareAndSet(ServiceState.STOPPED,ServiceState.WILL_START)
+				|| serviceState.compareAndSet(ServiceState.STOP_FAILED,ServiceState.WILL_START)) {
+			beforeStart();
+			if (serviceState.compareAndSet(ServiceState.WILL_START, ServiceState.STARTING)) {
+				try {
+					doStart();
+					serviceState.set(ServiceState.STARTED);
+					afterStart();
+				} catch (Exception e) {
+					serviceState.set(ServiceState.START_FAILED);
+					logger.error("启动失败", e);
+					stop();
+				}
+			}else{
+				throw new RuntimeException("启动失败,当前服务器状态为:"+ serviceState.get() + ",预期的状态为 WILL_START");
 			}
+		}else{
+			throw new RuntimeException("启动失败,当前服务器状态为:"+ serviceState.get() + ",预期的状态为 STOPPED");
 		}
 	}
 
 	@Override
 	public final void stop() {
-		// 设置状态将要关闭
-		serviceState.set(ServiceState.WILL_STOP);
 
-		beforeStop();
+		if(isStarted()) {
+			// 设置状态将要关闭
+			serviceState.set(ServiceState.WILL_STOP);
 
-		if (started.compareAndSet(true, false)) {
-			serviceState.set(ServiceState.STOPPING);
-			doStop();
-			serviceState.set(ServiceState.STOPPED);
-			afterStop();
+			beforeStop();
+
+			if(serviceState.compareAndSet(ServiceState.WILL_STOP,ServiceState.STOPPING)){
+				try {
+					doStop();
+				}catch (Exception e) {
+					serviceState.set(ServiceState.STOP_FAILED);
+					throw new RuntimeException(e);
+				}
+				serviceState.set(ServiceState.STOPPED);
+			}else {
+				throw new RuntimeException("关闭失败,当前服务器状态为:"+ serviceState.get() + " ,无法关闭");
+			}
+		}else {
+			throw new RuntimeException("关闭失败,当前服务器状态为:"+ serviceState.get() + " ,无法再次关闭");
 		}
-
 
 	}
 
 	@Override
 	public final boolean isStarted() {
-		//若没有启动，直接返回false
-		if (started.get()) {
+
 			switch (serviceState.get()) {
 				case WILL_STOP:
 				case STOPPING:
+				case STOP_FAILED:
 				case STOPPED:
 					return false;
 				default:
 					return true;
 			}
-		}
-		return false;
+
 	}
 
 	/**
--- iris-commons/src/main/java/com/cy/iris/commons/service/ServiceState.java
+++ iris-commons/src/main/java/com/cy/iris/commons/service/ServiceState.java
@@ -7,29 +7,42 @@ public enum ServiceState {
 	/**
 	 * 准备启动
 	 */
-	WILL_START,
+	WILL_START("WILL_START"),
 	/**
 	 * 启动中
 	 */
-	STARTING,
+	STARTING("STARTING"),
 	/**
 	 * 启动失败
 	 */
-	START_FAILED,
+	START_FAILED("START_FAILED"),
 	/**
 	 * 启动完成
 	 */
-	STARTED,
+	STARTED("STARTED"),
 	/**
 	 * 准备关闭
 	 */
-	WILL_STOP,
+	WILL_STOP("WILL_STOP"),
 	/**
 	 * 关闭中
 	 */
-	STOPPING,
+	STOPPING("STOPPING"),
+	/**
+	 * 关闭失败
+	 */
+	STOP_FAILED("STOP_FAILED"),
 	/**
 	 * 关闭完成
 	 */
-	STOPPED
+	STOPPED("STOPPED");
+
+	private String description;
+	ServiceState(String description){
+		this.description = description;
+	}
+
+	public String toString(){
+		return this.description;
+	}
 }
