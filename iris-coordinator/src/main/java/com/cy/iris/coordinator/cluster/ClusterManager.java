f2aa3119db2645a1ad3ea8f3fad108f9dabdfe33
chicc999 <123695646@qq.com>
2017-02-16 20:15:54 +0800

    cluster manager初始化

---
+++ iris-coordinator/src/main/java/com/cy/iris/coordinator/cluster/ClusterManager.java
@@ -0,0 +1,40 @@
+package com.cy.iris.coordinator.cluster;
+
+import com.cy.iris.commons.service.Service;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Created by cy on 17/2/16.
+ */
+public class ClusterManager extends Service{
+
+	private static final Logger logger = LoggerFactory.getLogger(ClusterManager.class);
+
+	@Override
+	public void beforeStart() throws Exception {
+
+	}
+
+	@Override
+	public void doStart() throws Exception {
+
+	}
+
+	@Override
+	public void afterStart() throws Exception {
+
+	}
+
+	@Override
+	public void beforeStop() {
+
+	}
+
+	@Override
+	public void doStop() {
+
+	}
+
+
+}
