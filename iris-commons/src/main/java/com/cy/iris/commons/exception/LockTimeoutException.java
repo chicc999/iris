b994ab71d912c4730f96f7bc1a6f4ba8090c6855
chicc999 <123695646@qq.com>
2017-03-09 16:18:24 +0800

    获取锁超时的异常

---
+++ iris-commons/src/main/java/com/cy/iris/commons/exception/LockTimeoutException.java
@@ -0,0 +1,22 @@
+package com.cy.iris.commons.exception;
+
+/**
+ * Created by cy on 17/3/9.
+ */
+public class LockTimeoutException extends Exception{
+	public LockTimeoutException() {
+	}
+
+	public LockTimeoutException(String message) {
+		super(message);
+	}
+
+	public LockTimeoutException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public LockTimeoutException(Throwable cause) {
+		super(cause);
+	}
+
+}
