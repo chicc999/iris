147102f20c903d8e2105436ae6f3539335fa34b0
chicc999 <123695646@qq.com>
2017-03-09 16:23:52 +0800

    带超时的读写锁方法

--- iris-commons/src/main/java/com/cy/iris/commons/util/lock/ZookeeperReadWriteLocks.java
+++ iris-commons/src/main/java/com/cy/iris/commons/util/lock/ZookeeperReadWriteLocks.java
@@ -1,10 +1,13 @@
 package com.cy.iris.commons.util.lock;
 
+import com.cy.iris.commons.exception.LockTimeoutException;
 import com.cy.iris.commons.service.Service;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.recipes.locks.InterProcessMutex;
 import org.apache.curator.framework.recipes.locks.InterProcessReadWriteLock;
 import org.apache.zookeeper.CreateMode;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * 基于zk实现的分布式读写锁
@@ -25,7 +28,15 @@ public class ZookeeperReadWriteLocks extends Service{
 		this.client = client;
 	}
 
-	public final Object get(String dataPath) throws Exception {
+	/**
+	 * 获取指定路径下的数据
+	 * 如果获取不到锁会一直阻塞
+	 * @param dataPath 待获取数据所在的路径
+	 * @return 指定路径下的数据
+	 *
+	 * @throws Exception 其他异常
+	 */
+	public final byte[] get(String dataPath) throws Exception {
 		this.readLock.acquire();
 		try {
 			return getDataByPath(dataPath);
@@ -34,8 +45,56 @@ public class ZookeeperReadWriteLocks extends Service{
 		}
 	}
 
+	/**
+	 * 获取指定路径下的数据
+	 * @param dataPath 待获取数据所在的路径
+	 * @param timeout  最多等待时间,单位ms
+	 * @return 指定路径下的数据
+	 *
+	 * @throws LockTimeoutException 获取锁超时
+	 * @throws Exception 其他异常
+	 */
+	public final byte[] get(String dataPath,long timeout) throws Exception {
+		if(!this.readLock.acquire(timeout, TimeUnit.MILLISECONDS)){
+			throw new LockTimeoutException();
+		}
+		try {
+			return getDataByPath(dataPath);
+		}finally {
+			this.readLock.release();
+		}
+	}
+
+	/**
+	 * 将指定路径修改为指定值
+	 * @param dataPath 数据的路径
+	 * @param value 数据的值
+	 *
+	 * @throws Exception
+	 */
 	public final void set(String dataPath,byte[] value) throws Exception {
 		this.writeLock.acquire();
+		try{
+			setDataByPath(dataPath,value);
+		}finally {
+			this.writeLock.release();
+		}
+	}
+
+	/**
+	 *
+	 * 将指定路径修改为指定值
+	 * @param dataPath 数据的路径
+	 * @param value 数据的值
+	 * @param timeout 获取锁的最大等待时间,单位ms
+	 *
+	 * @throws LockTimeoutException 获取锁超时
+	 * @throws Exception
+	 */
+	public final void set(String dataPath,byte[] value,long timeout) throws Exception {
+		if(!this.writeLock.acquire(timeout,TimeUnit.MILLISECONDS)){
+			throw new LockTimeoutException();
+		}
 		try{
 			setDataByPath(dataPath,value);
 		}finally {
@@ -47,7 +106,7 @@ public class ZookeeperReadWriteLocks extends Service{
 		client.setData().forPath(dataPath,value);
 	}
 
-	private Object getDataByPath(String dataPath) throws Exception {
+	private byte[] getDataByPath(String dataPath) throws Exception {
 		return client.getData().forPath(dataPath);
 	}
 
