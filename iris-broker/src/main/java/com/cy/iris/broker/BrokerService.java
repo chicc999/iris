a2d45b28fc55f359e376884c673efdd36955b2e5
chicc999 <123695646@qq.com>
2017-03-22 19:22:33 +0800

    broker初始化

---
+++ iris-broker/src/main/java/com/cy/iris/broker/BrokerService.java
@@ -0,0 +1,35 @@
+package com.cy.iris.broker;
+
+import com.cy.iris.commons.service.Service;
+
+/**
+ * @Author:cy
+ * @Date:Created in 18:36 17/3/22
+ * @Destription:
+ */
+public class BrokerService extends Service{
+	@Override
+	public void beforeStart() throws Exception {
+
+	}
+
+	@Override
+	public void doStart() throws Exception {
+
+	}
+
+	@Override
+	public void afterStart() throws Exception {
+
+	}
+
+	@Override
+	public void beforeStop() {
+
+	}
+
+	@Override
+	public void doStop() {
+
+	}
+}
