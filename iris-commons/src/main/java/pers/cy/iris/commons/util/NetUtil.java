cc51e31a530e5e101818d113c079842666055f38
chicc999 <123695646@qq.com>
2017-04-14 15:57:11 +0800

    添加address转16进制方法

--- iris-commons/src/main/java/pers/cy/iris/commons/util/NetUtil.java
+++ iris-commons/src/main/java/pers/cy/iris/commons/util/NetUtil.java
@@ -13,16 +13,16 @@ public class NetUtil {
 	 * 从serverName获取对应的InetSocketAddress.
 	 * serverName格式为xxx_xxx_xxx_xxx,如10_9_1_133
 	 *
-	 * @param address serverName格式的字符串
+	 * @param serverName serverName格式的字符串
 	 * @return inetSocketAddress
 	 * @throws UnknownHostException 如果遇到无法转换的serverName
 	 * @see java.net.InetSocketAddress
 	 */
-	public static InetSocketAddress serverNameToISA(String address) throws UnknownHostException {
+	public static InetSocketAddress serverNameToISA(String serverName) throws UnknownHostException {
 
-		ArgumentUtil.isNotBlank(address);
+		ArgumentUtil.isNotBlank(serverName);
 
-		String[] parts = address.split("_");
+		String[] parts = serverName.split("_");
 		if (parts.length < 5) {
 			throw new RuntimeException();
 		}
@@ -44,6 +44,45 @@ public class NetUtil {
 		InetSocketAddress isa = new InetSocketAddress(InetAddress.getByName(ip), port);
 
 		return isa;
+	}
+
+	/**
+	 * 把字节数组转化成16进制字符串
+	 *
+	 * @param address 字节数组
+	 * @return 16进制
+	 */
+	public static String toHex(byte[] address) {
+		StringBuilder builder = new StringBuilder();
+		appendHex(address, builder);
+		return builder.toString();
+	}
+
+	/**
+	 * 把字节数组转化成16进制字符串
+	 *
+	 * @param address 字节数组
+	 * @param builder 字符串构造器
+	 */
+	public static void appendHex(byte[] address, StringBuilder builder) {
+		if (address == null || address.length < 4) {
+			throw new IllegalArgumentException("serverName is invalid");
+		}
+		if (builder == null) {
+			throw new IllegalArgumentException("builder is invalid");
+		}
+		String hex;
+		int pos = 0;
+
+		for (int i = 0; i < 4; i++) {
+			hex = Integer.toHexString(address[pos++] & 0xFF).toUpperCase();
+			if (hex.length() == 1) {
+				builder.append('0').append(hex);
+			} else {
+				builder.append(hex);
+			}
+		}
+
 	}
 
 }
