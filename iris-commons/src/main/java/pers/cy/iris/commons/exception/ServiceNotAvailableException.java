7d62330b0969b73a24bea44ca68b5ca74075bb18
chicc999 <123695646@qq.com>
2017-04-25 19:41:09 +0800

    完善服务不可用异常

--- iris-commons/src/main/java/pers/cy/iris/commons/exception/ServiceNotAvailableException.java
+++ iris-commons/src/main/java/pers/cy/iris/commons/exception/ServiceNotAvailableException.java
@@ -3,8 +3,25 @@ package pers.cy.iris.commons.exception;
 /**
  * @Author:cy
  * @Date:Created in  17/4/22
- * @Destription:
+ * @Destription: 服务不可用异常
  */
-public class ServiceNotAvailableException extends Exception{
+public class ServiceNotAvailableException extends Exception {
 
+	private final static String description = "服务不可用";
+
+	public ServiceNotAvailableException() {
+		super(description);
+	}
+
+	public ServiceNotAvailableException(String detail) {
+		super(description + detail);
+	}
+
+	public ServiceNotAvailableException(String detail, Throwable cause) {
+		super(description + detail, cause);
+	}
+
+	public ServiceNotAvailableException(Throwable cause) {
+		super(description, cause);
+	}
 }
