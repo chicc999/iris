b6c7f0d80d4b1fae71a67866f8650eda0d9b6f7b
chicc999 <123695646@qq.com>
2017-05-14 20:50:04 +0800

    修改参数校验函数返回值

--- iris-commons/src/main/java/pers/cy/iris/commons/util/ArgumentUtil.java
+++ iris-commons/src/main/java/pers/cy/iris/commons/util/ArgumentUtil.java
@@ -12,18 +12,18 @@ public class ArgumentUtil {
 	 * 检测参数是否为null,如果为null则抛出空指针异常
 	 * 否则返回参数
 	 */
-	public static <T> T isNotNull(String text, T arg) {
+	public static <T> boolean isNotNull(String text, T arg) {
 		if (arg == null) {
 			throw new NullPointerException(text + " is null.");
 		}
-		return arg;
+		return true;
 	}
 
 	/**
 	 * 检测若干参数是否为null,如果为null则抛出空指针异常
 	 * 否则返回参数
 	 */
-	public static <T> void isNotNull(String[] text, T... arg) {
+	public static <T> boolean isNotNull(String[] text, T... arg) {
 		if (text.length != arg.length) {
 			throw new IllegalArgumentException("text[] 与 args 长度不等");
 		}
@@ -32,6 +32,7 @@ public class ArgumentUtil {
 			if (arg == null) {
 				throw new NullPointerException(text[i] + "is null.");
 			}
+			return true;
 	}
 
 	/**
