833d0413409058bcf8f916d6961c878138b8af15
chicc999 <123695646@qq.com>
2018-02-08 15:27:13 +0800

    添加接口

--- iris-broker/src/main/java/pers/cy/iris/broker/store/Store.java
+++ iris-broker/src/main/java/pers/cy/iris/broker/store/Store.java
@@ -13,6 +13,44 @@ import pers.cy.iris.commons.service.LifeCycle;
 public interface Store extends LifeCycle {
 
 	/**
+	 * 返回队列最大偏移量
+	 *
+	 * @param topic   主题
+	 * @param queueId 队列序号
+	 * @return 队列最大偏移量
+	 */
+	long getQueueMaxOffset(String topic, short queueId);
+
+
+	/**
+	 * 返回队列最小偏移量
+	 *
+	 * 注意此时如果日志文件已经被删除，最小偏移量对应的消息实际内容可能已经被删除
+	 *
+	 * @param topic   主题
+	 * @param queueId 队列序号
+	 * @return 队列最小偏移量
+	 */
+	long getQueueMinOffset(String topic, short queueId);
+
+	/**
+	 * 返回日志最大偏移量
+	 *
+	 * @return 日志最大偏移量
+	 */
+	long getJournalMaxOffset();
+
+	/**
+	 * 返回日志最小偏移量
+	 *
+	 * @return 日志最小偏移量
+	 */
+	long getJournalMinOffset();
+
+
+
+
+	/**
 	 * 生产消息
 	 *
 	 * @param message 消息
