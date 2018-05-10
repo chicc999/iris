ba23d0ebd4d0fea1b5c10bea8a561cf047b010a5
chicc999 <123695646@qq.com>
2018-05-10 20:14:58 +0800

    搭建测试环境

---
+++ iris-broker/src/test/java/pers/cy/iris/broker/store/file/TestChannelFile.java
@@ -0,0 +1,31 @@
+package pers.cy.iris.broker.store.file;
+
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+/**
+ * @Author:cy
+ * @Date:Created in  18/5/10
+ * @Destription: channelFile测试类
+ */
+public class TestChannelFile {
+
+	private ChannelFile channelFile;
+
+	@BeforeTest
+	public void beforeTest(){
+
+	}
+
+	@AfterTest
+	public void afterTest(){
+
+	}
+
+	@Test
+	public void testAppend(){
+
+	}
+
+}
